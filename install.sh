51c966fa639d758f361581dbc7f08c510393afc6
Naveen Donepudi <51416869+naveendonepudi1419@users.noreply.github.com>
2020-08-17 17:15:59 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+echo""
+echo -e "${GREEN}   "
+echo " _   _    _    ____ _  _______ ____   __        _____  ____  _     ____   "
+echo "| | | |  / \  / ___| |/ / ____|  _ \  \ \      / / _ \|  _ \| |   |  _ \  "
+echo "| |_| | / _ \| |   | ' /|  _| | |_) |  \ \ /\ / / | | | |_) | |   | | | | "
+echo "|  _  |/ ___ \ |___| . \| |___|  _ <    \ V  V /| |_| |  _ <| |___| |_| | "
+echo "|_| |_/_/   \_\____|_|\_\_____|_| \_\    \_/\_/  \___/|_| \_\_____|____/  "
+echo "                                                                          "
+
+
+echo -e "${RED}                                                      ~ Tools for Hacking by D.Naveen ${NC}"
+echo ""
+echo -e "${YELLOW} Twitter.com/naveendonepudi1419 | Instagram.com/naveendonepudi1419 | Github.com/naveendonepudi1419 ${NC} "
+echo ""
+echo "---------------------------------------------------------------------------------------"
+echo ""
+echo -e "${RED}[!] This Tool Must Run As ROOT [!]${NC}"
+echo ""
+echo -e "${CYAN}[>] Press ENTER to Install hackerworld, CTRL+C to Abort.${NC}"
+read INPUT
+echo ""
+
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/hackerworld"
+    BIN_DIR="$PREFIX/usr/bin/"
+    pkg install -y git python2
+else
+    INSTALL_DIR="/usr/share/doc/hackerworld"
+    BIN_DIR="/usr/bin/"
+fi
+
+echo "[✔] Checking directories...";
+if [ -d "$INSTALL_DIR" ]; then
+    echo "[!] A Directory hackerworld Was Found.. Do You Want To Replace It ? [y/n]:" ;
+    read mama
+    if [ "$mama" = "y" ]; then
+        rm -R "$INSTALL_DIR"
+    else
+        exit
+    fi
+fi
+
+echo "[✔] Installing ...";
+echo "";
+git clone https://github.com/naveendonepudi1419/hackerworld.git "$INSTALL_DIR";
+echo "#!/bin/bash
+python $INSTALL_DIR/hackerworld.py" '${1+"$@"}' > hackerworld;
+chmod +x hackerworld;
+sudo cp hackerworld /usr/bin/;
+rm hackerworld;
+
+
+if [ -d "$INSTALL_DIR" ] ;
+then
+    echo "";
+    echo "[✔] Successfuly Installed !!! [✔]";
+    echo "";
+    echo "[✔]========================================================================[✔]";
+    echo "[✔] ✔✔✔ All Is Done!! you can execute tool by typing hackerworld !! ✔✔✔ [✔]";
+    echo "[✔]========================================================================[✔]";
+    echo "";
+else
+    echo "[✘] Installation Failed !!! [✘]";
+    exit
+fi
---
+++ update.sh
@@ -0,0 +1,58 @@
+#
+#  
+# 
+#  _   _    _    ____ _  _______ ____   __        _____  ____  _     ____  
+# | | | |  / \  / ___| |/ / ____|  _ \  \ \      / / _ \|  _ \| |   |  _ \ 
+# | |_| | / _ \| |   | ' /|  _| | |_) |  \ \ /\ / / | | | |_) | |   | | | |
+# |  _  |/ ___ \ |___| . \| |___|  _ <    \ V  V /| |_| |  _ <| |___| |_| |
+# |_| |_/_/   \_\____|_|\_\_____|_| \_\    \_/\_/  \___/|_| \_\_____|____/ 
+                                                                        
+
+
+#                        ~ Tools For Hacking by D.Naveen ~
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackerworld/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/naveendonepudi1419
+
+clear
+
+mkdir naveendonepudi1419
+
+clear
+
+cd naveendonepudi1419
+
+clear
+
+git clone https://github.com/naveendonepudi1419/hacker-world.git
+
+clear
+
+cd hackerworld
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
